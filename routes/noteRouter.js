dece2b19594eac4e797f4cbcb7f12b04bf9ffa11
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-01-06 18:21:03 -0500

    added some things to notesrouter

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const {readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
+const {} = require('../helpers/uuid');
 
-const router = require('express').Router();
-const {readFromFile}
 
-const writeNote = fs
+// GET route to retrieve notes
+app.get('/public/notes.html', (req, res) => {
+    console.info(`${req.method} request recieved for notes`);
+    readFromFile('/public/notes.html').then((data) => res.json(JSON.parse(data)));
+  });
+  app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} has added a note`);
+    const 
+  })
+    
