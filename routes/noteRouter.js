04f825e575e4536c1fdaa279fd95ea0309fd4fab
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-01-06 18:13:41 -0500

    commit before I start working tonight

---
+++ routes/noteRouter.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const {readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
+
+const router = require('express').Router();
+const {readFromFile}
+
+const writeNote = fs
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+const uuid = require('./helpers/uuid')
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Note Taker');
+  res.sendFile(path.join(__dirname, '../notes-app/public/index.html'));
 });
 
-app.listen(PORT);
+app.get('/api/')
+
+app.listen(PORT, () => {
+    console.log(`app listening at http://localhost:${PORT}`);
+})
