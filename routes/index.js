ec353bba52aa42cf11b4ae572ebfdaa1b2806f03
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-01-06 21:40:27 -0500

    been working on this a while. Might take a break for the night and submit it and continue working on it over the weekend.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const notesRouter = require('./notesRouter');
 
+
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, './public/index.html'))
 );
